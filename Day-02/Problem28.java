6f84b1dc0eeb0d92dea1c3e48536e3ffcbfbc92e
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-06-16 20:58:57 +0530

    feat: 21-30 problems solved

---
+++ Day-02/Problem28.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Problem28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            int counter = 1;
+            while (counter < number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+
+    }
+}
